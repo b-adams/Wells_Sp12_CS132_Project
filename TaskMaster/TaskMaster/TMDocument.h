9aa1f98fdc4ae5d036fe0c59f1767f294678703d
Bryant E Adams <badams@wells.edu>
2012-05-01 17:20:51 -0400

    Add Document-based non-core-Data Cocoa Application

---
+++ TaskMaster/TaskMaster/TMDocument.h
@@ -0,0 +1,13 @@
+//
+//  TMDocument.h
+//  TaskMaster
+//
+//  Created by Bryant Adams on 5/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TMDocument : NSDocument
+
+@end
---
+++ TaskMaster/TaskMaster/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TaskMaster
+//
+//  Created by Bryant Adams on 5/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
