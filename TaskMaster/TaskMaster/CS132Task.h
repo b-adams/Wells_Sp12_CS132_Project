efd16fd9981217acf27e1b0c6367d04f02ca6f14
Bryant E Adams <badams@wells.edu>
2012-05-07 18:06:20 -0400

    Add shell of CS132Task.m. Still needs properties, methods (with stubs), documentation, and eventually real implementation.

---
+++ TaskMaster/TaskMaster/CS132Task.h
@@ -0,0 +1,13 @@
+//
+//  CS132Task.h
+//  TaskMaster
+//
+//  Created by Bryant Adams on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CS132Task : NSObject
+
+@end
---
+++ TaskMaster/TaskMaster/CS132Task.m
@@ -0,0 +1,13 @@
+//
+//  CS132Task.m
+//  TaskMaster
+//
+//  Created by Bryant Adams on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CS132Task.h"
+
+@implementation CS132Task
+
+@end
