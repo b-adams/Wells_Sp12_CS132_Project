ccd73919cb19646229f6d02fc3cf38e977d540c6
Bryant E Adams <badams@wells.edu>
2012-05-09 16:55:22 -0400

    Add method declarations and properties for Manager

--- TaskMaster/TaskMaster/CS132Manager.h
+++ TaskMaster/TaskMaster/CS132Manager.h
@@ -2,12 +2,29 @@
 //  CS132Manager.h
 //  TaskMaster
 //
-//  Created by Bryant Adams on 5/9/12.
+//  Created by Bryant Adams on 5/6/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#define ROOT_INDEX 1
+@class CS132Heap;
+@class CS132Task;
+
 
 @interface CS132Manager : NSObject
+@property (strong) NSMutableArray* tasksCompleted;
+@property (strong) CS132Heap* tasksToDo;
+@property (weak) CS132Task* topTask;
+-(void) fixTopTask;
 
+-(void) spendTimeOnTopTask:(int) seconds;
+-(void) changeRemainingTimeTo:(int) seconds;
+
+-(void) addTaskWithName: (NSString*) newName
+         andDescription: (NSString*) newDesc
+                 andDue: (NSDate*) newDue
+        andTimeRequired: (int) newTimeNeeded;
+
+-(int) countTasksToDo;
 @end
